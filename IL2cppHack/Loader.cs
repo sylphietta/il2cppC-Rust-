47d6c40fad782bee1dc9f81fce6603b887a9404c
Sylphietta <vorlhello@gmail.com>
2025-04-04 16:57:48 +0300

    Add files via upload

---
+++ IL2cppHack/Loader.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.InteropServices;
+using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+//created by sylphietta | write me in telegram: t.me/sylphydev
+namespace IL2cppHack
+{
+    [BepInPlugin("com.boosterfps", "FpsBooster", "1.0.0")]
+    public class Loader : BasePlugin
+    {
+        public override void Load()
+        {
+          InitializeMemory();
+          UnityEngine.Object.DontDestroyOnLoad(base.AddComponent<Manager>().gameObject);
+        }
+
+        private void InitializeMemory()
+        {
+            Harmony harmony = new Harmony("com.boosterfps");
+            harmony.PatchAll();
+        }
+    }
+}
